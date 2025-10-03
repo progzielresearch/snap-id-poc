f7232356ccc938b1289614fa6189eebd87951683
Progziel Technologies <progziel@gmail.com>
2025-10-03 14:02:34 +0500

    Update CORS

--- backend.py
+++ backend.py
@@ -32,7 +32,7 @@ import math
 app = FastAPI(title="Frontalizer (Gemini 2.5 Flash Image)")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://194.59.165.64:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
